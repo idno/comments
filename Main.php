1757e4f35b9318e2d7fe5cf29d27de63b97cb05f
Marcus Povey <marcus@marcus-povey.co.uk>
2019-08-11 18:24:55 +0100

    Moving Comments from core

---
+++ Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IdnoPlugins\Comments {
+
+    use Idno\Common\Plugin;
+
+    class Main extends Plugin
+    {
+
+        function RegisterPages()
+        {
+
+            \Idno\Core\Idno::site()->routes()->addRoute('/comments/post/?', '\IdnoPlugins\Comments\Pages\Post', true);
+            \Idno\Core\Idno::site()->template()->extendTemplate('entity/annotations/comment/main', 'comments/public/form');
+        }
+
+        function registerTranslations()
+        {
+
+            \Idno\Core\Idno::site()->language()->register(
+                new \Idno\Core\GetTextTranslation(
+                    'comments', dirname(__FILE__) . '/languages/'
+                )
+            );
+        }
+
+    }
+
+}
